278de165d53e11deb8eeb79ead1bf82f548afa9c
DONADEI Emrick <emrick.donadei@hotmail.fr>
2020-09-04 22:59:32 +0200

    removed base CSS and fixed tw issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="mt-10">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-4">
+      <p className="text-gray-700 text-4xl">Hello from Tailwind CSS</p>
     </div>
   );
 }
